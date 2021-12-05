78b141445dc575c1633d0c422f9087bf0aaa38a0
Vitico99 <nenevictor99@gmail.com>
2021-12-05 21:05:22 +0100

    Change placeable definition and added adyacentEnemy
    Change placeable for frontier since its more accurate

--- src/board.pl
+++ src/board.pl
@@ -1,12 +1,13 @@
 :- module(board, []).
 
-:- dynamic bug/5. % bug(Player, Type of Bug, X pos, Y pos, Stack pos)
-:- dynamic placeable/2. %placeable(X,Y): can place a new bug on cell X,Y
-:- dynamic visited/2. % Check if cell(X,Y) has been visited by dfs
+:- dynamic bug/5. % bug(Color, Type of Bug, X pos, Y pos, Stack pos)
+:- dynamic frontier/2. % frontier(X,Y): cell(X,Y) is a frontier cell, this is an empty cell that is adyacent to a bug of the hive.
+:- dynamic visited/2. % visited(X,Y): cell(X,Y) has been visited by dfs.
 
 init_board():-
-    assert(placeable(0,0)).
+    assert(frontier(450,390)).
 
+% Adyacent definition for an hexagonal grid
 adyacent(X1,Y1,X2,Y2):- X2 is X1 - 1, Y2 is Y1.
 adyacent(X1,Y1,X2,Y2):- X2 is X1 -1 , Y2 is Y1 + 1.
 adyacent(X1,Y1,X2,Y2):- X2 is X1, Y2 is Y1 - 1. 
@@ -17,31 +18,41 @@ adyacent(X1,Y1,X2,Y2):- X2 is X1 + 1, Y2 is Y1.
 empty(X,Y):- \+ (bug(_,_,X,Y,_)).
 
 emptyAdyacent(X1, Y1, X2, Y2):-
-    adyacent(X1, Y1, X2, Y2), empty(X2, Y2).
+    adyacent(X1, Y1, X2, Y2), 
+    empty(X2, Y2).
 
 nonEmptyAdyacent(X,Y, X1, Y1):-
     adyacent(X,Y,X1,Y1), 
     \+empty(X1,Y1).
 
-placeableAdyacent(X1, Y1, X2, Y2):-
+frontierAdyacent(X1, Y1, X2, Y2):-
     adyacent(X1, Y1, X2, Y2),
-    placeable(X2, Y2).
+    frontier(X2, Y2).
+
+adyacentOpponent(X1, Y1, X2, Y2, C1):-
+    adyacent(X1, Y1, X2, Y2),
+    bug(C2, _, X2, Y2, _),
+    C1 \== C2.
 
 isIsolated(X,Y):-
-    findall([X1,Y1],nonEmptyAdyacent(X,Y, X1,Y1),L),
+    findall([X1,Y1], nonEmptyAdyacent(X,Y, X1,Y1),L),
     length(L, 1).
     
 isolatedEmptyAdyacent(X1, Y1, X2, Y2):- % X2, Y2 is only adyacnet to X1,Y1
     emptyAdyacent(X1,Y1,X2,Y2),
     isIsolated(X2,Y2).
 
-placeBug(Player,Type,X,Y):-
-    assert(bug(Player,Type,X,Y, 0)), retract(placeable(X,Y)),
-    forall(emptyAdyacent(X,Y,X1,Y1), assert(placeable(X1, Y1))).
+placeBug(C,T,X,Y):-
+    assert(bug(C,T,X,Y,0)), retract(frontier(X,Y)), 
+    forall(emptyAdyacent(X,Y,X1,Y1), assert(frontier(X1, Y1))). % expand the frontier of the hive
 
 getAllPlaceableCells(PlaceablePositions):-
     findall([X,Y],placeable(X,Y),PlaceablePositions).
 
+% Checks if a bug of Color C can be placed at cell (X,Y)
+placeableByColor(X,Y,C):-
+    frontier(X,Y), 
+    \+ adyacentOpponent(X,Y,_,_,C).
 
 getBug(X,Y, S, bug(P,T,X,Y,S)):-%Get the bug in Position X,Y with stack number S
     bug(P,T,X,Y,S).
